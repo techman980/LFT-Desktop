69583be65e7e1b7c3debeef0c309a762001cfcfd
AluminiumTech <aluminiumdev@gmail.com>
2017-04-08 23:59:52 +0800

    fixed logo issues and about.html

--- main.js
+++ main.js
@@ -4,17 +4,18 @@ const url = require('url')
 
 let win;
 
+var iconPath = path.join(__dirname, 'images/logo.png');
 
 function createWindow()
 {
 win = new BrowserWindow({
     width:1280, 
     height:720,
-icon: __dirname + 'img/Logo_100.png'
+icon: iconPath
 });
 
 win.loadURL(url.format({
-  pathname: path.join(__dirname, 'src/index.html'),
+  pathname: path.join(__dirname, 'index.html'),
   protocol: 'file',
   slashes: 'true'
 
